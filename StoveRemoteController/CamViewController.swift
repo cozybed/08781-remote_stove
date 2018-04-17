302fa6923e0737147a507450bc9d301b44d646db
Zhexi Liu <zhexil@andrew.cmu.edu>
2018-04-17 17:05:59 -0400

    Added web view

---
+++ StoveRemoteController/CamViewController.swift
@@ -0,0 +1,40 @@
+//
+//  CamViewController.swift
+//  StoveRemoteController
+//
+//  Created by zhexi liu on 4/17/18.
+//  Copyright © 2018 zhexi liu. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class CamViewController: UIViewController {
+
+    @IBOutlet weak var webview: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let url = URL(string:"https://www.youtube.com/")
+        let request = URLRequest(url: url!)
+        webview.load(request)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
