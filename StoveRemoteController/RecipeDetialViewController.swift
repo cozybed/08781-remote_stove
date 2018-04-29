be263852ae3bea2d7a81ed0c7e2c86785d00efb1
Zhexi Liu <zhexil@andrew.cmu.edu>
2018-04-29 19:25:16 -0400

    Cleanup and temporiarly remove edit button

--- StoveRemoteController/RecipeDetialViewController.swift
+++ StoveRemoteController/RecipeDetialViewController.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 
-
 class RecipeDetialViewController: UIViewController {
 
     
@@ -19,55 +18,32 @@ class RecipeDetialViewController: UIViewController {
     @IBOutlet weak var descLabel: UITextView!
     
     /*
-    @IBAction func testSave(_ sender: Any) {
+    func testSave(_ sender: Any) {
         
-        //recipe_arr
         //DataManager.save (recipe_arr[0], with: "testRecipie.log")
-        
         var all_data = DataManager.loadAll( RecipeItem.self)
         print (all_data)
-        
-        var blah = ""
-        for data in all_data {
-            blah += data.to_string()
-        }
-        
-        descLabel.text  = blah
-        
     }*/
     
     @IBAction func deleteRecipeButtonClicked(_ sender: Any) {
        
-        var cur_recipe = recipe_arr[myIndex]
-        
-        
-        print ("Current index is:")
-        print (myIndex)
+        let cur_recipe = recipe_arr[myIndex]
         DataManager.delete(cur_recipe.id.uuidString)
-        //recipe_arr.remove(at: myIndex)
-        //myIndex = 0
-        
-        
         let next:UIViewController = (self.storyboard?.instantiateViewController(withIdentifier: "all_recipe_board"))!
-        
         self.present(next, animated: true, completion: nil)
-        
-        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        
-        var cur_recipe = recipe_arr[myIndex]
+        let cur_recipe = recipe_arr[myIndex]
         recipeTitleLabel.text = cur_recipe.name
         var steps_str = ""
         
         for s in cur_recipe.steps {
             steps_str += s.to_string()
         }
-        
         stageTextView.text = steps_str
         descLabel.text = cur_recipe.description
     }
@@ -76,16 +52,4 @@ class RecipeDetialViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
